a497b7b1914c2f69ada75c27c361e7cbc9314b6d
chriiiis24 <chrisalvaradomartinez@gmail.com>
2025-12-15 01:05:54 -0600

    Agregar archivos de proyecto.

---
+++ Data/InventarioTechSolutionsDbContext.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using InventarioTechSolutions.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace InventarioTechSolutions.Data;
+
+public partial class InventarioTechSolutionsDbContext : DbContext
+{
+    public InventarioTechSolutionsDbContext()
+    {
+    }
+
+    public InventarioTechSolutionsDbContext(DbContextOptions<InventarioTechSolutionsDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Producto> Productos { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseSqlServer("Name=ConnectionStrings:DefaultConnection");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Producto>(entity =>
+        {
+            entity.HasKey(e => e.IdProducto).HasName("PK__Producto__09889210600832D8");
+
+            entity.HasIndex(e => e.Nombre, "IX_Productos_Nombre");
+
+            entity.Property(e => e.Nombre).HasMaxLength(100);
+            entity.Property(e => e.Precio).HasColumnType("decimal(18, 2)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
---
+++ Models/Producto.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace InventarioTechSolutions.Models;
+
+public partial class Producto
+{
+    public int IdProducto { get; set; }
+
+    public string Nombre { get; set; } = null!;
+
+    public decimal Precio { get; set; }
+
+    public int CantidadStock { get; set; }
+}
